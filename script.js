e1f2f306daa19de795e94d4401267cb49d675021
NitturiRaju <saikrishnabbsskk@gmail.com>
2026-07-25 14:19:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,43 +11,43 @@ const svg = document.getElementById("overlay");
 
 document.querySelectorAll(".hotspot").forEach(circle => {
 
-    circle.addEventListener("click", function () {
+  circle.addEventListener("click", function () {
 
-        const social = [...this.classList].find(c => c !== "hotspot");
+    const social = [...this.classList].find(c => c !== "hotspot");
 
-        // Remove previous ripple
-        svg.querySelectorAll(".ripple").forEach(r => r.remove());
+    // Remove previous ripple
+    svg.querySelectorAll(".ripple").forEach(r => r.remove());
 
-        // Ripple animation
-        const ripple = document.createElementNS(
-            "http://www.w3.org/2000/svg",
-            "circle"
-        );
+    // Small ripple
+    const ripple = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
 
-        ripple.setAttribute("cx", this.getAttribute("cx"));
-        ripple.setAttribute("cy", this.getAttribute("cy"));
-        ripple.setAttribute("r", "20");
-        ripple.setAttribute("class", "ripple");
+    ripple.setAttribute("cx", this.getAttribute("cx"));
+    ripple.setAttribute("cy", this.getAttribute("cy"));
+    ripple.setAttribute("r", "10"); // Smaller pop
+    ripple.setAttribute("class", "ripple");
 
-        svg.appendChild(ripple);
+    svg.appendChild(ripple);
 
-        // Google Analytics
-        if (typeof gtag === "function") {
-            gtag("event", "social_click", {
-                social_platform: social
-            });
-        }
+    // Google Analytics
+    if (typeof gtag === "function") {
+      gtag("event", "social_click", {
+        social_platform: social
+      });
+    }
 
-        // Small vibration
-        if (navigator.vibrate) {
-            navigator.vibrate(15);
-        }
+    // Small vibration
+    if (navigator.vibrate) {
+      navigator.vibrate(10);
+    }
 
-        // Open link
-        setTimeout(() => {
-    window.open(links[social], "_blank");
-}, 150);
+    // Always navigate
+    setTimeout(() => {
+      location.assign(links[social]);
+    }, 50);
 
-    });
+  });
 
 });
